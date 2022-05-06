6195edec5e6af52e9b4ab943da1c3fc3dcbeb96d
pchavan <pchavan@pieriandx.com>
2022-05-06 16:14:44 +0530

    Added MA and Volcano python files

---
+++ Assignment/src/Volcano.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import glob
+from bioinfokit import analys, visuz
+import re 
+
+mapath = "C:\Python_files\Snakemake_practice\Assignment\results\MA"
+# C:\Python_files\Snakemake_practice\Assignment\data
+# C:\Python_files\Snakemake_practice\Assignment\results\MA
+for i in glob.glob("C:\Python_files\Snakemake_practice\Assignment\data\*.csv"):
+    name = re.search("data\\\(.*)\.csv",str(i))
+    print(i)
+    name = name.group(1)
+    df = pd.read_csv(i)
+    df.head()
+    df = df.dropna()
+    # visuz.GeneExpression.ma(df=df, lfc='log2FoldChange', ct_count='lfcSE', st_count='lfcSE', pv='pvalue',plotlegend=True,figname=r'C:\\Python_files\\Snakemake_practice\\Assignment\\results\\MA\\'+name)
+    visuz.GeneExpression.volcano(df=df, lfc='log2FoldChange', pv='pvalue',figname=r'C:\\Python_files\\Snakemake_practice\\Assignment\\results\\Volcano\\'+name)
